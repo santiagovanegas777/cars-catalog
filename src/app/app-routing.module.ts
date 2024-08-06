1af178bb0f3ca4e6f80cbd2f4a640a68cb81bc19
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2024-08-06 13:28:18 +0200

    authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth-guard.guard';
 
 const routes: Routes = [
   {
@@ -28,6 +29,7 @@ const routes: Routes = [
   },
   {
     path: `cars`,
+    canActivate: [AuthGuard],
     loadChildren: () => import('./pages/cars/cars.module').then(m => m.CarsModule)
   },
 ];
