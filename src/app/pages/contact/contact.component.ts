657da5ba62bcf7ab354c030ac2b7df9612781e80
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2024-09-19 20:51:45 +0200

    componente contacto email js funcional

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_edi86rv', 'template_dway8kg', e.target as HTMLFormElement, {
+        publicKey: 'fccmKogGdVyzolAc-',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
+
 
 }
